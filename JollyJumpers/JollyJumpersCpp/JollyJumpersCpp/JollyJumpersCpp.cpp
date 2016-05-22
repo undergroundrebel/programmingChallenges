223acdc6f7554ddbbc2348c9bbf471e9764070d6
undergroundrebel <michellevogel820@gmail.com>
2016-05-21 17:32:29 -0700

    stopping point for the evening. Compiles, runs and passes my 2 tests. Does not pass the judge though.

--- JollyJumpers/JollyJumpersCpp/JollyJumpersCpp/JollyJumpersCpp.cpp
+++ JollyJumpers/JollyJumpersCpp/JollyJumpersCpp/JollyJumpersCpp.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAXSIZE 3000
 using namespace std;
@@ -12,15 +14,49 @@ using namespace std;
 
 int main()
 {
-	int i, numInts, state, var[MAXSIZE];
-	bool absVal[MAXSIZE] = { 0 };
-	string input = "";
-	long long numInts, a, b;
-	while (cin){
+	int numInts = 0, state =1, intA, intB, intC, var[MAXSIZE] = { 0 };
+	bool absVal[MAXSIZE] = { 0 };	
+	string jolly = "Jolly"; 
+	string notJolly = "Not Jolly";
+	while (cin) {
 		cin >> numInts;
-	}
-	
+		long long i = 0;
+			while (i < numInts)
+			{
+				cin >> var[i];
+				i++;
+			}
+			for (int i = 0; i < numInts; i++) {
+				absVal[abs(var[i] - var[i + 1])] = 1;
+			}
 
+			/*	while (i < numInts) {
+					cin >> intA;
+					i++;
+					if (i < numInts) {
+						cin >> intB;
+						intC = abs(intA - intB);
+						absVal[intC] = 1;
+					}
+				}*/
+			state = 1;
+			for (i = 1; i < (numInts); i++) {
+				if (!absVal[i])
+				{
+					state = 0;
+					break;
+
+				}
+
+			}
+			if (state)
+				cout << jolly << endl;
+			else
+				cout << notJolly << endl;
+		}
+
+
+	
 	return 0;
 }
  
