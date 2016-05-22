0e67a738b663acad811fa770d92e6318e1996b02
undergroundrebel <michellevogel820@gmail.com>
2016-05-22 12:05:57 -0700

    removed 1 for loop, one array and started the process of adding in error handling. Still not correctly handling the case where the number of integers is greater than the number stated.

--- JollyJumpers/JollyJumpersC/JollyJumpersC/main.c
+++ JollyJumpers/JollyJumpersC/JollyJumpersC/main.c
@@ -1,51 +1,67 @@
-//#ifdef _MSC_VER
-//#define _CRT_SECURE_NO_WARNINGS
-//#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #define MAXSIZE 3000
+
+void printJolly(int amI);
+
 main()
 {
 	//
 	// Dave:  The Abs array can be of type bool -- it might also be better named since it has the same name as the abs function
 	// Michelle: done
 	//
-	int numInts, i, a, Var[MAXSIZE];
+	int numInts = 0, i = 0, intA = 0, intB = 0, jollyNotJolly = 0;
 	bool AbsValue[MAXSIZE] = { 0 };
 
-	while (scanf_s("%d", &numInts) == 1) {				//while not end of file
-		//
-		// Dave:  You shouldn't need a pass to read in the numbers and a second pass to calculate the difference.
-		//        Reading in the values and calculating the difference can be done in one pass.
-		// Michelle: Will fix in the c++ & C# versions.
-		//
-		for (i = 0; i < numInts; i++)					// for the number of items on the line
-		{
-			scanf_s("%d", &Var[i]);				//shove that number into the variable array
-			AbsValue[i] = 0;							//set the memory for it in the absolute array to 0 - no one likes bad memory bits
-		}
-		for (i = 0; i < (numInts - 1); i++) {
-			AbsValue[abs(Var[i] - Var[i + 1])] = 1;	// loop through the var array, store a 1 at the location of the absolute difference between the values
-		}
-
-		//
-		// Dave:  This might be cleaner with the test of Abs[i] in the conditional of the for loop and then check to see if 
-		//        i equals n after the loop to determine Jolly.
-		// Michelle: not understanding what you are suggesting. 
-		//
-		a = 1;
-		for (i = 1; i < numInts; i++) {
-			if (!AbsValue[i])
+	while (scanf_s("%d", &numInts) == 1) //while not end of file
+		{		
+			if (numInts > 3000 || numInts < 0 || numInts == 0 )
 			{
-				a = 0;
-				break;
+				while ((i = getchar()) != '\n') 
+				{
+					jollyNotJolly = 0;
+				}
 			}
-		}
-		if (a)
-			printf("Jolly\n");
-		else
-			printf("Not jolly\n");
+			else 
+			{
+				for (i = 0; i < numInts; i++)
+				{
+					scanf_s("%d", &intA);
+					if (i != 0)
+					{
+						AbsValue[abs(intA - intB)] = 1;
+					}
+					intB = intA;
+				}
+				//
+				// Dave:  This might be cleaner with the test of Abs[i] in the conditional of the for loop and then check to see if 
+				//        i equals n after the loop to determine Jolly.
+				// Michelle: not understanding what you are suggesting. 
+				//
+				jollyNotJolly = 1;
+				for (i = 1; i < numInts; i++)
+				{
+					if (!AbsValue[i])
+					{
+						jollyNotJolly = 0;
+						break;
+					}
+				}
+			
+			}
+			printJolly(jollyNotJolly);
+	}
+}
+
+void printJolly(int amI)
+{
+	if (amI)
+	{
+		printf("Jolly\n");
+	}
+	else
+	{
+		printf("Not jolly\n");
 	}
 }
